84d01862f3027fad94181c5ffbbe0af4c54ac5b1
Elias Breitenbach <elias.b@me.com>
2023-04-11 18:49:44 +0200

    Create a Project

---
+++ Day26_Pj4_Stepper/Day26_Pj4_StepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day26_Pj4_StepperApp.swift
+//  Day26_Pj4_Stepper
+//
+//  Created by Elias Breitenbach on 11.04.23.
+//
+
+import SwiftUI
+
+@main
+struct Day26_Pj4_StepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Day26_Pj4_StepperUITests/Day26_Pj4_StepperUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Day26_Pj4_StepperUITestsLaunchTests.swift
+//  Day26_Pj4_StepperUITests
+//
+//  Created by Elias Breitenbach on 11.04.23.
+//
+
+import XCTest
+
+final class Day26_Pj4_StepperUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
